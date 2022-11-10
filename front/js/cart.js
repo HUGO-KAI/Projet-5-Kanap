32f09d27da3dcf9e8d4250f5e4fd126eb1101dd8
hugo_tan <tank5460@hotmail.fr>
2022-11-10 11:34:38 +0100

    etape 9 update

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,11 +1,12 @@
 const urlProducts = `http://localhost:3000/api/products`;
 container = document.getElementById('cart__items');
+var localProducts = JSON.parse(localStorage.getItem("localProducts"))
+init()
+
 function init () {
-    
-    var localProducts = JSON.parse(localStorage.getItem("localProducts"))
     if (localProducts == null || localProducts.length == 0){
-        container.style = 'display:none';
-        document.querySelector ('h1').textContent = 'Votre panier est vide'
+        /*container.style = 'display:none';*/
+        document.querySelector ('h1').textContent = 'Votre panier est vide';
     }
     else {
         let itemsQuantity = 0;
@@ -19,11 +20,11 @@ function init () {
                                         <div class="cart__item__content__description">
                                             <h2>${localProducts[i].name}</h2>
                                             <p>${localProducts[i].colors}</p>
-                                            <p>${localProducts[i].price}</p>
+                                            <p>${parseFloat(localProducts[i].price)} €</p>
                                         </div>
                                         <div class="cart__item__content__settings">
                                             <div class="cart__item__content__settings__quantity">
-                                            <p>${localProducts[i].quantity}</p>
+                                            <p>Qté : ${localProducts[i].quantity}</p>
                                             <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${localProducts[i].quantity}>
                                             </div>
                                             <div class="cart__item__content__settings__delete">
@@ -33,11 +34,28 @@ function init () {
                                         </div>
                                     </article>`;
             itemsQuantity += localProducts[i].quantity;
-            itemsPrice += localProducts[i].quantity * parseInt(localProducts[i].price);
+            itemsPrice += localProducts[i].quantity * parseFloat(localProducts[i].price);
         }
         document.getElementById('totalQuantity').textContent = itemsQuantity;
         document.getElementById('totalPrice').textContent = itemsPrice;
         
     }
+    const supprimer = document.getElementsByClassName('deleteItem');
+    
+    
 }
-init()
+
+const el = document.querySelector(".cart__item");
+/*const el = document.querySelector(".cart__item");
+for (let i = 0; i < el.length; i++){
+    console.log(el[i].dataset.id + el[i].dataset.color );
+}*/
+function deleteItem (evt) {
+    if (evt.target.closest(".deleteItem")){
+        el.style = "display: none"
+    }
+}
+
+el.addEventListener("click",deleteItem );
+ 
+
