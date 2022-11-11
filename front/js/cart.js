48e52cfa830b7ffa19aa5063f19f8754857f5ccf
hugotan <tank5460@hotmail.fr>
2022-11-11 23:35:32 +0100

    etape 10 update

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,11 +1,14 @@
+
 const urlProducts = `http://localhost:3000/api/products`;
 container = document.getElementById('cart__items');
 var localProducts = JSON.parse(localStorage.getItem("localProducts"))
-init()
 
+
+/*Afficher les canapés choisis par utilisateur dans la page panier*/
+init()
 function init () {
     if (localProducts == null || localProducts.length == 0){
-        /*container.style = 'display:none';*/
+        
         document.querySelector ('h1').textContent = 'Votre panier est vide';
     }
     else {
@@ -41,8 +44,9 @@ function init () {
        
     }
    }
-const supprimerButtons = document.querySelectorAll(".deleteItem");
 
+/*Permettre aux utilisateurs de supprimer les items dans le panier*/ 
+const supprimerButtons = document.querySelectorAll(".deleteItem");
 supprimerButtons.forEach(function(supprimerButton){
     supprimerButton.addEventListener("click",function(){
         let supprimerId = supprimerButton.closest(".cart__item").getAttribute('data-id');
@@ -58,6 +62,7 @@ supprimerButtons.forEach(function(supprimerButton){
     })
 })
 
+/*Permettre aux utilisateurs de modifier la quantité des items présent dans le panier*/ 
 const changeQuantityInputs = document.querySelectorAll(".itemQuantity");
 
 changeQuantityInputs.forEach(function(changeQuantityInput){
@@ -74,6 +79,126 @@ changeQuantityInputs.forEach(function(changeQuantityInput){
     })
 })
 
-    
+/*Valider l'information saisi par acheteur dans le formulaire*/
+const form = document.querySelector('.cart__order__form');
+
+//valider input prénom
+let nameRegExp = new RegExp (/^[a-zA-Z ,.'-éèàë]+$/i);
+form.firstName.addEventListener('change', function(){
+    valideFirstName(this);
+});
+const valideFirstName = function(inputFirstName){
+    let test = nameRegExp.test(inputFirstName.value);
+    if (test == false) {
+        document.getElementById("firstNameErrorMsg").textContent = "Prénom non valide";
+        return false;
+    }
+    else {
+        document.getElementById("firstNameErrorMsg").textContent = "Prénom valide";
+        return true;
+    }
+};
+
+//valider input nom
+form.lastName.addEventListener('change', function(){
+    valideLastName(this);
+});
+const valideLastName = function(inputLastName){
+    let test = nameRegExp.test(inputLastName.value);
+    if (test == false) {
+        document.getElementById("lastNameErrorMsg").textContent = "Nom non valide";
+        return false;
+    }
+    else {
+        document.getElementById("lastNameErrorMsg").textContent = "Nom valide";
+        return true;
+    }
+};
+
+// valider input ville
+form.city.addEventListener('change', function(){
+    valideCity(this);
+});
+const valideCity = function(inputVille){
+    let test = nameRegExp.test(inputVille.value);
+    if (test == false) {
+        document.getElementById("cityErrorMsg").textContent = "Ville non valide";
+        return false;
+    }
+    else {
+        document.getElementById("cityErrorMsg").textContent = "Ville valide";
+        return true;
+    }
+};
+
+//valider input adresse
+let adresseRegExp = new RegExp (
+    '[a-zA-Z0-9]'
+);
+form.address.addEventListener('change', function(){
+    valideAdresse(this);
+});
+const valideAdresse = function(inputAdresse){
+    let test = adresseRegExp.test(inputAdresse.value);
+    if (test == false) {
+        document.getElementById("addressErrorMsg").textContent = "L'adresse non valide";
+        return false;
+    }
+    else {
+        document.getElementById("addressErrorMsg").textContent = "L'adresse valide";
+        return true;
+    }
+};
+
+//valider email
+var emailRegExp = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/;
+form.email.addEventListener('change', function(){
+    valideEmail(this);
+});
+const valideEmail = function(inputEmail){
+   let testEmail = emailRegExp.exec(inputEmail.value);
+    if (testEmail == null || testEmail == false) {
+        document.getElementById("emailErrorMsg").textContent = "Email non valide";
+        return false;
+    }
+    else {
+        document.getElementById("emailErrorMsg").textContent = "Email valide";
+        return true;
+    }
+};
+
+console.log(form.firstName.value)
+/*Constituer un objet contact (à partir des données du formulaire)*/
+const urlOrder = `http://localhost:3000/api/products/order`;
+let getId = localProducts.map(product => product.id);
 
 
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+    let contact = {
+        "firstName":form.firstName.value,
+        "lastName":form.lastName.value,
+        "address":form.address.value,
+        "city":form.city.value,
+        "email":form.email.value
+    };
+    console.log(contact);
+    console.log(getId);
+    if (valideFirstName(form.firstName) && valideLastName(form.lastName)  && valideAdresse(form.address) && valideCity(form.city) && valideEmail(form.email)){
+        
+        const promise = fetch(urlOrder, {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json', 
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                contact,
+                products : getId
+                
+            }),
+            
+        })
+       
+    }
+});
